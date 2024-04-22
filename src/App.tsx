67815f95e4d013197535970994332aed062bf792
noexpect9 <1909293822@qq.com>
2024-04-22 16:42:54 +0800

    feat: github actions

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <main>
-        <div className='w-[21cm] min-h-[29.7cm] overflow-auto mx-auto my-0 p-[1em]'>大大说</div>
+        <div className='w-[21cm] min-h-[29.7cm] overflow-auto mx-auto my-0 p-[1em]'>TEST</div>
         <Demo />
       </main>
     </>
