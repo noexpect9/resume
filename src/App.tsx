b3885aa1ed641e23cb99d0a5c6aa95515e341e59
noexpect9 <1909293822@qq.com>
2024-04-22 14:23:03 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Demo from './components/Demo'
+function App() {
+
+  return (
+    <>
+      <main>
+        <div className='w-[21cm] min-h-[29.7cm] overflow-auto mx-auto my-0 p-[1em]'>大大说</div>
+        <Demo />
+      </main>
+    </>
+  )
+}
+
+export default App
